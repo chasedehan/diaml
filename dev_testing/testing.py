b29f769eae0cbba766e28b797977de8d8d22fa10
Chase DeHan <chase.dehan@progleasing.com>
2017-12-28 16:25:27 -0700

    First iteration of imputer and new missing column created

--- dev_testing/testing.py
+++ dev_testing/testing.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import numpy as np
 import urllib
+
 
 ########################################################################################################################
 #
@@ -16,14 +18,30 @@ raw_data = urllib.request.urlopen(labels_url)
 labels = pd.read_csv(raw_data, delimiter=",", header=None)
 labels.columns = ["Y"]
 
+
+train.iloc[1:4,1:10] = np.NaN
 ########################################################################################################################
 #
 #  Test that imputer is working
 #
 ########################################################################################################################
+from imputer.imputer import NewMissingColumn, DataFrameImputer
 
-im = ImputeMissing()
-im.fit(train)
-new_x = im.transform(train)
-#or
-new_x = im.fit_transform(train)
+new_cols = NewMissingColumn(cutoff=0)
+new_cols.fit(train)
+new_X = new_cols.transform(train)
+new_X.shape
+
+#impute values according to user specifications
+dfi = DataFrameImputer(cont_impute="mode")
+dfi.fit(train)
+new_X = dfi.transform(train)
+
+#So, stringing these together we see:
+from sklearn.pipeline import Pipeline
+missing_pipeline = Pipeline([('new_cols', NewMissingColumn(cutoff=0)),
+                             ('impute', DataFrameImputer(cont_impute="mean"))])
+new_X = missing_pipeline.fit_transform(train)
+new_X.shape
+
+
--- imputer/imputer.py
+++ imputer/imputer.py
@@ -1,77 +1,110 @@
 import pandas as pd
 import numpy as np
-from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.base import TransformerMixin
 import warnings
+import scipy.stats as ss
 
 ########################################################################################
 #
-# Main Class and Methods
+# Create New Variables with
 #
 ########################################################################################
 
-class ImputeMissing(BaseEstimator, TransformerMixin):
 
-    def __init__(self, cutoff=0.05, cont_impute="median", ohe=True, silent=False, ignore_vars=[]):
+class NewMissingColumn(TransformerMixin):
+
+    def __init__(self, cutoff=0.02, ohe=True, silent=False):
         self.cutoff = cutoff
-        self.cont_impute = cont_impute
         self.ohe = ohe
         self.silent = silent
-        self.ignore_vars = ignore_vars
 
         # Throw errors if the inputted parameters don't meet the necessary criteria
-        if (cutoff <= 0) | (cutoff >=1):
+        if (cutoff < 0) | (cutoff >= 1):
             raise ValueError('cutoff should be between 0 and 1. You entered' + str(cutoff))
-
-        # Issue warnings for parameters to still let it run
-        if(cont_impute not in ["mean", "median", "most_frequent"]):
-            #Changes to something that will run, but does raise an error to user
-            self.cont_impute = "median"
-            warnings.warn("WARNING: " + str(cont_impute) + " is not a valid value. Reverting back to 'median'")
 
     def fit(self, X, y=None):
         #Does not require a y, but will accept it to conform to sklearn methods
         #Check if there are any NA values
         if X.isnull().values.any():
-            self.new_df = _imputer(X)  #TODO: this isn't quite right, what do we want to return?
+            self._new_column_id(X)
+            # if self.new_missing_column:
+            #     self._new_column_id(X)
         else:
-            self.new_df = X
-            warnings.warn("No NA values in dataframe")
+            warnings.warn("No NA values in dataframe. Process Finished")
         return self
 
     def transform(self, X):
+        #call new_missing_columns
+        new_columns = [j + "_IsMissing" for j in self.new_missing_columns]
+        for i in range(len(new_columns)):
+            this_column = self.new_missing_columns[i]
+            this_new_column = new_columns[i]
+            #Create new column by coercing old boolean column into 1/0
+            X[this_new_column] = X[this_column].isnull() * 1
+        #call ohe if required
         return X
 
-    def fit_transform(self, X, y=None):
-        self.fit(X)
-        return self.transform(X)
+    def _new_column_id(self, X):
+        nrow = X.shape[1]
+        #Find the missing columns
+        missing_columns = X.columns[X.isnull().any()]
+
+        ########Create the new columns for those with missing values
+        # if above threshold, create new columns
+        self.new_missing_columns = []
+        for i in missing_columns:
+            if (sum(X[i].isnull()) / nrow) > self.cutoff:
+                self.new_missing_columns.append(i)
+        return self
+
 
 
 ########################################################################################
 #
-# Helper Functions to do the Heavy Lifting
+# Impute values - median for categorical, user choice for
 #
 ########################################################################################
-
-def _imputer(X):
-
-    #What columns have missing values?
-    missing_columns = []
+#https://stackoverflow.com/questions/25239958/impute-categorical-missing-values-in-scikit-learn
 
 
-    ########Create the new columns for those with missing values
-    #if above threshold, create new columns
-    new_columns = []
+class DataFrameImputer(TransformerMixin):
+
+    def __init__(self, cont_impute="median"):
+        """Impute missing values.
+        Columns of dtype object are imputed with the most frequent value in column.
+        Columns of other types are imputed with user selected (median as default) of column.
+        """
+
+        self.cont_impute = cont_impute
+        if(cont_impute not in ["mean", "median", "mode"]):
+            #Changes to something that will run, but does raise an error to user
+            self.cont_impute = "median"
+            warnings.warn("WARNING: " + str(cont_impute) + " is not a valid value. Reverting back to 'median'")
+
+    def fit(self, X, y=None):
+        def set_fill(self, X):
+            self.fill = pd.Series([X[c].value_counts().index[0]
+                                   if X[c].dtype == np.dtype('O') else self.this_fun(X[c]) for c in X],
+                                  index=X.columns)
+
+        #Set the imputer desired for each of the imputer values desired
+            #This is super hacky because of trying to pull out the differing fill methods
+        if self.cont_impute == 'mode':
+            holder_fun = getattr(ss, 'mode')
+            self.this_fun = lambda x: holder_fun(x)[0][0] #This is weird, but it works (probably slow)
+            set_fill(self, X)
+        else:
+            self.this_fun = getattr(np, self.cont_impute)
+            if self.cont_impute == 'median':
+                self.fill = pd.Series([X[c].value_counts().index[0]
+                                  if X[c].dtype == np.dtype('O') else X[c].median() for c in X],
+                                  index=X.columns)
+            else:
+                set_fill(self, X)
+
+        return self
 
 
-    #######Split the dataframe apart to impute the variables
-    #Get datatypes for each column
+    def transform(self, X, y=None):
+        return X.fillna(self.fill)
 
-    #split columns into two groups
-
-    #impute according to user input
-
-    #Bind back together
-
-
-    ######Save transformations - TODO: how do I do this?
-    return X #TODO: what do I return here?
